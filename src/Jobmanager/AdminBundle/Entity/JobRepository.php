95c5f82d0b928a5da223f9fad8149cb4f2768dea
gerard <pa@foulquier.info>
2014-04-27 16:26:56 +0200

    refs #490 : refacto setCompanyToJob et setCompanyToContact as method from JobRepository

--- src/Jobmanager/AdminBundle/Entity/JobRepository.php
+++ src/Jobmanager/AdminBundle/Entity/JobRepository.php
@@ -69,46 +69,44 @@ class JobRepository extends EntityRepository
      */
     public function setCompanyToJob($em, $job_import, $job)
     {
-//        // ** for the new job created create a company if not exist
-//        // ** attach the company to the job
-//
-//        // retrieve company name from company entity
-//        $company_import = $job_import->getCompany();
-//        $company = $em->getRepository('JobmanagerAdminBundle:Company')
-//            ->findByName($company_import);
-//
-//        // check if company exists
-//        if (empty($company)) {
-//
-//            // if not exists create a new one
-//            $company = new Company();
-//            $company->setName($job_import->getCompany());
-//            $company->setZip($job_import->getZip());
-//            $company->setCity($job_import->getCity());
-//            $company->setCountry('France');
-//            $company->setUrlCompany($job_import->getUrlCompany());
-//
-//            // set contact to company
-//            $contact = $this->setContactToCompany($em, $job_import, $company);
-//
-//            // attache to job
-//            $job->setCompany($company);
-//
-//            // persist
-//            $em->persist($company);
-//            $em->persist($contact);
-//
-//            return $company;
-//
-//        } else {
-//
-//            // if exists attach to job
-//            $job->setCompany($company[0]);
-//
-//            return $company[0];
-//
-//        }
+        // ** for the new job created create a company if not exist
+        // ** attach the company to the job
 
+        // retrieve company name from company entity
+        $company_import = $job_import->getCompany();
+        $company = $em->getRepository('JobmanagerAdminBundle:Company')
+            ->findByName($company_import);
 
+        // check if company exists
+        if (empty($company)) {
+
+            // if not exists create a new one
+            $company = new Company();
+            $company->setName($job_import->getCompany());
+            $company->setZip($job_import->getZip());
+            $company->setCity($job_import->getCity());
+            $company->setCountry('France');
+            $company->setUrlCompany($job_import->getUrlCompany());
+
+            // set contact to company
+            $contact = $this->setContactToCompany($em, $job_import, $company);
+
+            // attache to job
+            $job->setCompany($company);
+
+            // persist
+            $em->persist($company);
+            $em->persist($contact);
+
+            return $company;
+
+        } else {
+
+            // if exists attach to job
+            $job->setCompany($company[0]);
+
+            return $company[0];
+
+        }
     }
 }
