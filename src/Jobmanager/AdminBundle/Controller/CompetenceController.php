fabe73e618ffed18c9fa95623640e882e6011b35
gerard <pa@foulquier.info>
2014-05-10 14:46:04 +0200

    refs #567 : CRUD Entité competence

---
+++ src/Jobmanager/AdminBundle/Controller/CompetenceController.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gerard
+ * Date: 09/05/2014
+ * Time: 21:35
+ */
+
+namespace Jobmanager\AdminBundle\Controller;
+
+use Jobmanager\AdminBundle\Form\CompetenceEditType;
+use Jobmanager\AdminBundle\Form\CompetenceType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Jobmanager\AdminBundle\Entity\Competence;
+
+class CompetenceController extends Controller
+{
+    public function indexAction()
+    {
+        // call entity manager
+        $em = $this->getDoctrine()->getManager();
+
+        // retrieve competences
+        $competences = $em->getRepository('JobmanagerAdminBundle:Competence')
+            ->findAll();
+
+        // send view
+        return $this->render('JobmanagerAdminBundle:Competence:index.html.twig', array(
+            'competences' => $competences
+        ));
+    }
+
+
+    public function createAction()
+    {
+
+        // new competence
+        $competence = new Competence();
+
+        // generate form
+        $form = $this->createForm(new CompetenceType, $competence);
+
+        // get request
+        $request = $this->get('request');
+
+        // check if post sent
+        if ($request->getMethod() == 'POST') {
+
+            // bind data form
+            $form->handleRequest($request);
+
+            // valid form
+            if ($form->isValid()) {
+
+                // save in db
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($competence);
+                $em->flush();
+
+                // send message
+                $this->get('session')->getFlashBag()->add('notice', 'Compétence enregistrée');
+
+                // redirect
+                return $this->redirect($this->generateUrl('admin_competence_index'));
+
+            }
+
+        }
+
+
+        // if no post
+        return $this->render('JobmanagerAdminBundle:Competence:create-edit-competence.html.twig', array(
+            'form' => $form->createView()
+        ));
+
+    }
+
+    public function viewAction(Competence $competence)
+    {
+        // call entity manager
+        $em = $this->getDoctrine()->getManager();
+
+        // send view
+        return $this->render('JobmanagerAdminBundle:Competence:view.html.twig', array(
+            'competence' => $competence
+        ));
+    }
+
+    public function editAction(Competence $competence)
+    {
+        // generate form
+        $form = $this->createForm(new CompetenceEditType, $competence);
+
+        // get request
+        $request = $this->get('request');
+
+        // check if post sent
+        if ($request->getMethod() == 'POST') {
+
+            // bind data form
+            $form->handleRequest($request);
+
+            // valid data
+            if ($form->isValid()) {
+
+                // call entity manager
+                $em = $this->getDoctrine()->getManager();
+
+                // persist and flush
+                $em->persist($competence);
+                $em->flush();
+
+                // send flas message
+                $this->get('session')->getFlashMessage()->add('info', 'Compétence modifié.');
+
+                // redirect
+                return $this->redirect($this->generateUrl('admin_competence_index'));
+
+            }
+        }
+
+        // send view
+        return $this->render('JobmanagerAdminBundle:Competence:create-edit-competence.html.twig', array(
+            'form' => $form->createView(),
+            'competence' => $competence
+        ));
+    }
+
+    public function deleteAction(Competence $competence)
+    {
+        // create empty form against csrf
+        $form = $this->createFormBuilder()->getForm();
+
+        // get request
+        $request = $this->get('request');
+
+        // check if post sent
+        if ($request->getMethod() == 'POST') {
+
+            // bind data form
+            $form->handleRequest($request);
+
+            // valide data
+            if ($form->isValid()) {
+
+                // call entity manager
+                $em = $this->getDoctrine()->getManager();
+
+                // remove competence
+                $em->remove($competence);
+                $em->flush();
+
+                // send flash message
+                $this->get('session')->getFlashBag()->add('info', 'Compétence supprimé.');
+
+                // redirect
+                return $this->redirect($this->generateUrl('admin_competence_index'));
+            }
+        }
+
+        // send view
+        return $this->render('JobmanagerAdminBundle:Competence:delete.html.twig', array(
+            'form' => $form->createView(),
+            'competence' => $competence
+        ));
+    }
+}
---
+++ src/Jobmanager/AdminBundle/Form/CompetenceEditType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Jobmanager\AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CompetenceEditType extends CompetenceType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Jobmanager\AdminBundle\Entity\Competence'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'jobmanager_adminbundle_competenceedittype';
+    }
+}
