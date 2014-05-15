c156bcca12b38de3ef2b4cba1bc4d4d5d1893018
gerard <pa@foulquier.info>
2014-05-15 20:30:30 +0200

    deploy inte script

--- install.sh
+++ install.sh
@@ -4,5 +4,4 @@ cap deploy:cold
 cap symfony:doctrine:database:drop
 cap symfony:doctrine:database:create
 scp -r web/uploads root@job.fresh.local:/var/www/sfblog/current/web/
-scp /home/pi/jobmanager.sql root@job.fresh.local:/home/gerard/
-mysql -h job.fresh.local -u root -pqqq jobmanager < /home/gerard/jobmanager.sql
+mysql -h job.fresh.local -u root -pqqq jobmanager < /home/pi/jobmanager.sql
