60773c1b174409c2b939db0d6473a554f995bd64
gerard <pa@foulquier.info>
2014-05-19 11:16:03 +0200

    correction chemin uploads

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ cap deploy:setup
 cap deploy:cold
 cap symfony:doctrine:database:drop
 cap symfony:doctrine:database:create
-scp -r web/uploads root@job.fresh.local:/var/www/sfblog/current/web/
+scp -r web/uploads root@job.fresh.local:/var/www/jobmanager/current/web/
 mysql -h job.fresh.local -u root -pqqq jobmanager < /home/pi/jobmanager.sql
