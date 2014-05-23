962e09d0da89dd1dcbee932c56f0d2a6f0873af0
gerard <pa@foulquier.info>
2014-05-23 23:40:18 +0200

    refs #622 : integration auth sha1

--- src/Jobmanager/UserBundle/DataFixtures/ORM/Users.php
+++ src/Jobmanager/UserBundle/DataFixtures/ORM/Users.php
@@ -11,9 +11,18 @@ namespace Jobmanager\UserBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Jobmanager\UserBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class Users implements FixtureInterface
+class Users implements FixtureInterface, ContainerAwareInterface
 {
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     public function load(ObjectManager $manager)
     {
         $noms = array('gerard');
@@ -21,8 +30,18 @@ class Users implements FixtureInterface
         foreach ($noms as $i => $nom) {
             $users[$i] = new User();
             $users[$i]->setUsername($nom);
-            $users[$i]->setPassword($nom);
-            $users[$i]->setSalt('');
+            $users[$i]->setSalt(uniqid(mt_rand()));
+
+            // call encoder service
+            $encoder = $this->container
+                ->get('security.encoder_factory')
+                ->getEncoder($users[$i])
+            ;
+
+            $password = $encoder->encodePassword('137paf6', $users[$i]->getSalt());
+
+            $users[$i]->setPassword($password);
+
             $users[$i]->setRoles(array('ROLE_ADMIN'));
 
             $manager->persist($users[$i]);
